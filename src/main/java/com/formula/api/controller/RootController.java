a713c96f429c67fb1fcad5e63734f4c3f9f32535
brian <briancorrea@gmail.com>
2019-09-29 19:43:28 -0400

    formatting

--- src/main/java/com/formula/api/controller/RootController.java
+++ src/main/java/com/formula/api/controller/RootController.java
@@ -3,8 +3,6 @@ package com.formula.api.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.formula.api.model.User;
-
 @RestController
 public class RootController {
 	
