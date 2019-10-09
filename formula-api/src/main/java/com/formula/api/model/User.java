0b3a89c08b7972f108f5adc015c26a5ea1a9fe81
brian <briancorrea@gmail.com>
2019-10-09 02:17:55 -0400

    User model

--- formula-api/src/main/java/com/formula/api/model/User.java
+++ formula-api/src/main/java/com/formula/api/model/User.java
@@ -1,13 +1,65 @@
 package com.formula.api.model;
 
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
-	private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	private String firstName;
+	private String lastName;
 	private String email;
 	private String password;
-	private String lastModified;
-	
+	private Date createdAt;
+	private Date updatedAt;
+
 	public void sayHi() {
 		System.out.println("sayHi");
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
 	}
 
 }
